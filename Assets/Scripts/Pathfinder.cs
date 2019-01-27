3741252a4d1c389d02b24a2f3f08a844f5401e16
martin <venturamartin90@gmail.com>
2019-01-26 22:31:19 -0300

    Pathfinding Dijkstra

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -1,52 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public static class Pathfinder {
 
-	public static List<INode> BFS(INode source, INode dest) {
+    static bool FilterNodes(INode node)
+    {
+        return true;
+    }
+
+
+    public static List<INode> BFS(INode source, INode dest) {
+
 		//initialization
 		var visited = new HashSet<INode>();// add contains remove
 		var parent = new Dictionary<INode, INode>();
-
-
-		parent[source] = null;
-
-
-		Queue<INode> nodeQueue = new Queue<INode>();
-		nodeQueue.Enqueue(source);
-
+        var distance = new Dictionary<INode, float>();
+        parent[source] = null;
+        List<INode> nodeList = new List<INode>();
+        nodeList.Add(source);
+        distance[source] = 0.0f;
 		INode current = null;
 
-		while(nodeQueue.Count != 0) {
-			//visit current top of the queue.
-			current = nodeQueue.Dequeue();
+		while(nodeList.Count != 0) {
+            //Calculate distances.
+            INode minNode = nodeList[0];
+            float minDistance = distance[minNode];
+            foreach (var node in nodeList)
+                if (distance.ContainsKey(node) && distance[node] < minDistance)
+                    minNode = node;
+            current = minNode;
+
+            //Remove swapping with last element.
+            int lastIndex = nodeList.Count - 1;
+            int midIndex = nodeList.FindIndex(ind => ind.Equals(minNode));
+            INode temp = nodeList[lastIndex];
+            nodeList[midIndex] = nodeList[lastIndex];
+			nodeList.Remove(nodeList[lastIndex]);
 
 			if(current == dest) break;
 
+            //Function to eventually filter nodes.
+            Func<INode, bool> filterNeighbors = new Func<INode, bool>(FilterNodes);
 
-
-
-			//add it's neighbors to the queue.
-			foreach(var node in current.neighbors)
-				if(!visited.Contains(node)) {
+            //Visit and add it's neighbors to the queue.
+            foreach (var node in current.neighbors)
+				if(!visited.Contains(node) && filterNeighbors(node)) {
 					visited.Add(node);
-					nodeQueue.Enqueue(node);
+                    distance[node] = distance[current] + (current.position - node.position).sqrMagnitude;
+                    nodeList.Add(node);
 					parent[node] = current;
 				}
 		}
 
-
-		//build path.
-		List<INode> path = new List<INode>();
+        //build path.
+        List<INode> path = new List<INode>();
 
 		while(current != source) {
 			path.Add(current);
 			current = parent[current];
 		}
 		path.Add(source);
-		path.Reverse();
-
+        //return source.position.
 		return path;
 	}
 
