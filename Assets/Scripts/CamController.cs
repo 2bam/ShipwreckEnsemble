f7089dab500664a38e795b3d1181b8c6073d2020
nit <contact@2bam.com>
2019-01-27 11:41:10 -0300

    Moves zooming to separate component CamController

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+	Camera _cam;
+	MainBuilding _main;
+	private void Start() {
+		_cam = GetComponent<Camera>();
+		_main = FindObjectOfType<MainBuilding>();
+	}
+
+	private void LateUpdate() {
+		var mult = 3f;
+
+		//https://answers.unity.com/questions/1231701/fitting-bounds-into-orthographic-2d-camera.html
+		var colliders = _main.GetComponentsInChildren<Collider2D>();
+		var bounds = new Bounds();
+		foreach(var col in colliders)
+			bounds.Encapsulate(col.bounds);
+
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+
+		float targetRatio = bounds.size.x / bounds.size.y;
+
+		if(screenRatio >= targetRatio) {
+			_cam.orthographicSize = mult * bounds.size.y / 2;
+		}
+		else {
+			float differenceInSize = targetRatio / screenRatio;
+			_cam.orthographicSize = mult * bounds.size.y / 2 * differenceInSize;
+		}
+
+		//transform.position = new Vector3(bounds.center.x, bounds.center.y, -1f);
+
+	}
+
+}
