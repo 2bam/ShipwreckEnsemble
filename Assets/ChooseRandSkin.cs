8da7e6ce490c20a82f287dbcae4863f31fbbb9f2
tozy <tozy@hotmail.com.ar>
2019-01-27 14:57:56 -0300

    Added functionallity to choose a random NPC from the pool of srpites.

---
+++ Assets/ChooseRandSkin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseRandSkin : MonoBehaviour
+{
+
+    public Sprite[] allSkins;
+    public SpriteRenderer myRenderer; //I know this is not cool but whatever.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer.sprite = allSkins.Choice();
+    }
+}
